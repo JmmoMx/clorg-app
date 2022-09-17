135b8122f733373246146e1ad95d903b71280410
JmmoMx <84398795+JmmoMx@users.noreply.github.com>
2022-09-17 11:38:06 -0500

    w

--- resources/views/infoinicial.blade.php
+++ resources/views/infoinicial.blade.php
@@ -51,7 +51,7 @@
                         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                 for="evSub">
-                                SubDirección
+                                SubDirección o Departamento
                             </label>
                             <div class="relative">
                                 <select
@@ -150,7 +150,7 @@
                         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                 for="adSub">
-                                SubDirección
+                                SubDirección o Departamento
                             </label>
                             <div class="relative">
                                 <select
